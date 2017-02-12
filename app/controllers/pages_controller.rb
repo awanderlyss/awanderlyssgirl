aee33671fa0aaa58499b95a2ef947842e3106e25
awanderlyss <alyssa.felix7@gmail.com>
2017-02-12 16:49:09 -0500

    Add show action to controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,7 @@
 class PagesController < ApplicationController
+
+  def show
+    # render a page template that is the same name as a parameter that is passed in
+    render template: "pages/#{params[:page]}"
+  end
 end
