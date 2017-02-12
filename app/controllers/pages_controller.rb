548de92b40d5adccfdd3815c086f0d46063a0155
awanderlyss <alyssa.felix7@gmail.com>
2017-02-12 17:26:13 -0500

    Update pages controller, for pages that does exist

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,15 @@
 class PagesController < ApplicationController
-
+  # http://blog.teamtreehouse.com/static-pages-ruby-rails
   def show
-    # render a page template that is the same name as a parameter that is passed in
-    render template: "pages/#{params[:page]}"
+    if valid_page?
+      render template: "pages/#{params[:page]}"
+    else
+      render file: "public/404.html", status: :not_found
+    end
+  end
+
+  private
+  def valid_page?
+    File.exist?(Pathname.new(Rails.root + "app/views/pages/#{params[:page]}.html.erb"))
   end
 end
